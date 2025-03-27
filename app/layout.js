23a95949ef1adce42d6fe24be7b069b40cbcb8d6
zurabiannn <ornella.zurabian@gmail.com>
2025-03-27 23:36:29 +0400

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Silent Return",
+  description: "Anonymous Lost & Found system for boarding schools",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
